bc98426ded5fb324a3b440fdcdb7fc56bd3cbae8
CHANDAN KUMAR <125118924+Chandan0034@users.noreply.github.com>
2024-04-12 01:03:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from flask_cors import CORS
 
@@ -12,9 +14,9 @@ def title():
     try:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(options=chrome_options)
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
         driver.get("https://google.com")
         return jsonify({"Message":driver.title})
     except Exception as e:
