1e89ae9047256ce71ac374eb69b36d8ce7299743
CHANDAN KUMAR <125118924+Chandan0034@users.noreply.github.com>
2024-04-12 00:57:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def title():
     try:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome()
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(options=chrome_options)
         driver.get("https://google.com")
         return jsonify({"Message":driver.title})
     except Exception as e:
