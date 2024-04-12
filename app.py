9e3e601e7af816509d1bd2772659252d856e0862
CHANDAN KUMAR <125118924+Chandan0034@users.noreply.github.com>
2024-04-12 15:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from flask_cors import CORS
-import youtube_dl
 
 app = Flask(__name__)
 CORS(app)
@@ -13,16 +16,12 @@ def home():
 def get_video_src():
     # Get the website link from the request query parameters
     website_link = request.args.get('website_link')
-    ydl_opts = {'quiet': True,'extract_flat': True,'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            result = ydl.extract_info(website_link, download=False)
-            if 'entries' in result:  # If multiple videos are found
-                return [video['url'] for video in result['entries']]
-            else:  # If only one video is found
-                return jsonify({"Video":result['url']})
-        except youtube_dl.utils.DownloadError as e:
-            return jsonify({"Error ":str(e)})
-
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(website_link)
+    return jsonify({"title":driver.title})
 if __name__ == '__main__':
     app.run(debug=True)
